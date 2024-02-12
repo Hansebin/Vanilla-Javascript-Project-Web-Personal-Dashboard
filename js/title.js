2b470a686c36e72b0a012671fab75be00c9fc4f5
Hansebin <sebinn052@gmail.com>
2024-02-12 18:38:05 +0900

    refactor: 즉시 실행 함수 사용하여 독립적인 스코프 가지도록 코드 수정

--- js/title.js
+++ js/title.js
@@ -1,49 +1,55 @@
-const loginForm = document.getElementById("login-from");
-const loginInput = document.querySelector("#login-from input");
-const titleContainer = document.getElementById("greeting-container");
-const title = document.getElementById("greeting");
-const titleBtn = titleContainer.querySelector("button");
-const toDoFormContainer = document.getElementById("todo-form-container");
+const title = (function () {
+  const HIDDEN_CLASS = "hidden";
+  const USERNAME_KEY = "username";
+  const getUserName = localStorage.getItem(USERNAME_KEY);
 
-const HIDDEN_CLASS = "hidden";
-const USERNAME_KEY = "username";
+  const elements = {
+    $loginForm: document.getElementById("login-from"),
+    $loginInput: document.querySelector("#login-from input"),
+    $titleContainer: document.getElementById("greeting-container"),
+    $title: document.getElementById("greeting"),
+    $titleBtn: document.querySelector("#greeting-container button"),
+    $toDoFormContainer: document.getElementById("todo-form-container"),
+  };
 
-const getUserName = localStorage.getItem(USERNAME_KEY);
+  const common = {
+    paintingTitle: function (username) {
+      elements.$titleContainer.classList.remove(HIDDEN_CLASS);
+      elements.$toDoFormContainer.classList.remove(HIDDEN_CLASS);
+      elements.$title.innerText = `Hello, ${username}!`;
+    },
+    playLoginForm: function () {
+      elements.$loginForm.classList.remove(HIDDEN_CLASS);
+      elements.$loginForm.addEventListener(
+        "submit",
+        handleEvent.submitLoginForm
+      );
+    },
+  };
 
-const titleCommon = {
-  paintingTitle: function (username) {
-    titleContainer.classList.remove(HIDDEN_CLASS);
-    toDoFormContainer.classList.remove(HIDDEN_CLASS);
-    title.innerText = `Hello, ${username}!`;
-  },
-  playLoginForm: function () {
-    loginForm.classList.remove(HIDDEN_CLASS);
-    loginForm.addEventListener("submit", handleEventTitle.submitLoginForm);
-  },
-};
+  const handleEvent = {
+    submitLoginForm: function (e) {
+      e.preventDefault();
 
-const handleEventTitle = {
-  submitLoginForm: function (e) {
-    e.preventDefault();
+      const usernameInput = elements.$loginInput.value;
+      localStorage.setItem(USERNAME_KEY, usernameInput);
 
-    const usernameInput = loginInput.value;
-    localStorage.setItem(USERNAME_KEY, usernameInput);
+      elements.$loginForm.classList.add(HIDDEN_CLASS);
+      common.paintingTitle(usernameInput);
+    },
+    clickRenameBtn: function () {
+      localStorage.removeItem(USERNAME_KEY);
+      elements.$titleContainer.classList.add(HIDDEN_CLASS);
+      elements.$toDoFormContainer.classList.add(HIDDEN_CLASS);
+      common.playLoginForm();
+    },
+  };
 
-    loginForm.classList.add(HIDDEN_CLASS);
-    titleCommon.paintingTitle(usernameInput);
-  },
-  clickRenameBtn: function () {
-    localStorage.removeItem(USERNAME_KEY);
-    titleContainer.classList.add(HIDDEN_CLASS);
-    toDoFormContainer.classList.add(HIDDEN_CLASS);
-    titleCommon.playLoginForm();
-  },
-};
-
-if (getUserName === null) {
-  titleCommon.playLoginForm();
-} else {
-  titleCommon.paintingTitle(getUserName);
-  toDoFormContainer.classList.remove(HIDDEN_CLASS);
-  titleBtn.addEventListener("click", handleEventTitle.clickRenameBtn);
-}
+  if (getUserName === null) {
+    common.playLoginForm();
+  } else {
+    common.paintingTitle(getUserName);
+    elements.$toDoFormContainer.classList.remove(HIDDEN_CLASS);
+    elements.$titleBtn.addEventListener("click", handleEvent.clickRenameBtn);
+  }
+})();

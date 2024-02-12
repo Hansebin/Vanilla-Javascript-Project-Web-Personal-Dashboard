608dd5978b477ea6c78e23dbdbd9620fec3071f9
Hansebin <sebinn052@gmail.com>
2024-02-12 18:37:22 +0900

    refactor: 즉시 실행 함수 사용하여 독립적인 스코프 가지도록 코드 수정

--- js/book_mark.js
+++ js/book_mark.js
@@ -1,86 +1,89 @@
-const bookMarkIcon = document.getElementById("book-mark-icon");
-const bookMarkContainer = document.getElementById("book-mark-list-container");
-const bookMarkForm = bookMarkContainer.querySelector("form");
-const urlNameInput = bookMarkContainer.querySelector("input");
-const urlInput = bookMarkContainer.querySelector("div > input");
-const bookMarkList = document.getElementById("book-mark-list");
+const bookMark = (function () {
+  const BOOKMARK_KEY = "book_marks";
+  const savedBookMarks = localStorage.getItem(BOOKMARK_KEY);
+  let bookMarks = [];
 
-const BOOKMARK_KEY = "book_marks";
+  const elements = {
+    $bookMarkIcon: document.getElementById("book-mark-icon"),
+    $bookMarkContainer: document.getElementById("book-mark-list-container"),
+    $bookMarkForm: document.querySelector("#book-mark-list-container form"),
+    $urlNameInput: document.querySelector("#book-mark-list-container input"),
+    $urlInput: document.querySelector("#book-mark-list-container div > input"),
+    $bookMarkList: document.getElementById("book-mark-list"),
+  };
 
-let bookMarks = [];
+  const common = {
+    paintingBookMark: function (urlObj) {
+      const li = document.createElement("li");
+      const div = document.createElement("div");
+      const iconSpan = document.createElement("span");
+      const a = document.createElement("a");
+      const deleteSpan = document.createElement("span");
 
-const bookMarkCommon = {
-  paintingBookMark: function (urlObj) {
-    const li = document.createElement("li");
-    const div = document.createElement("div");
-    const iconSpan = document.createElement("span");
-    const a = document.createElement("a");
-    const deleteSpan = document.createElement("span");
+      li.id = urlObj.id;
+      iconSpan.innerText = "★";
+      a.innerText = urlObj.name;
+      a.href = urlObj.url;
+      a.target = "_blank";
+      deleteSpan.innerText = "x";
+      deleteSpan.addEventListener("click", handleEvent.deleteBookMarkList);
 
-    li.id = urlObj.id;
-    iconSpan.innerText = "★";
-    a.innerText = urlObj.name;
-    a.href = urlObj.url;
-    a.target = "_blank";
-    deleteSpan.innerText = "x";
-    deleteSpan.addEventListener(
-      "click",
-      handleEventBookMark.deleteBookMarkList
-    );
+      div.appendChild(iconSpan);
+      div.appendChild(a);
+      li.appendChild(div);
+      li.appendChild(deleteSpan);
+      elements.$bookMarkList.appendChild(li);
+    },
+    savingBookMark: function () {
+      localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookMarks));
+    },
+    paintingSavedBookMarks: function () {
+      const parseBookMarks = JSON.parse(savedBookMarks);
+      if (parseBookMarks && parseBookMarks.length > 0) {
+        bookMarks = parseBookMarks;
+        parseBookMarks.forEach(this.paintingBookMark);
+      }
+    },
+  };
 
-    div.appendChild(iconSpan);
-    div.appendChild(a);
-    li.appendChild(div);
-    li.appendChild(deleteSpan);
-    bookMarkList.appendChild(li);
-  },
-  savingBookMark: function () {
-    localStorage.setItem(BOOKMARK_KEY, JSON.stringify(bookMarks));
-  },
-  paintingSavedBookMarks: function () {
-    const parseBookMarks = JSON.parse(savedBookMarks);
-    if (parseBookMarks && parseBookMarks.length > 0) {
-      bookMarks = parseBookMarks;
-      parseBookMarks.forEach(this.paintingBookMark);
-    }
-  },
-};
+  const handleEvent = {
+    clickIcon: function () {
+      elements.$bookMarkContainer.classList.toggle("hidden");
+    },
+    SubmitBookMarkForm: function (e) {
+      e.preventDefault();
 
-const handleEventBookMark = {
-  clickIcon: function () {
-    bookMarkContainer.classList.toggle("hidden");
-  },
-  SubmitBookMarkForm: function (e) {
-    e.preventDefault();
+      const newUrlName = elements.$urlNameInput.value;
+      const newUrl = elements.$urlInput.value;
 
-    const newUrlName = urlNameInput.value;
-    const newUrl = urlInput.value;
+      elements.$urlNameInput.value = "";
+      elements.$urlInput.value = "";
 
-    urlNameInput.value = "";
-    urlInput.value = "";
+      const urlObj = {
+        id: Date.now(),
+        name: newUrlName,
+        url: newUrl,
+      };
 
-    const urlObj = {
-      id: Date.now(),
-      name: newUrlName,
-      url: newUrl,
-    };
+      bookMarks.push(urlObj);
+      common.paintingBookMark(urlObj);
+      common.savingBookMark();
+    },
+    deleteBookMarkList: function (e) {
+      const li = e.target.parentElement;
+      const id = parseInt(li.id);
 
-    bookMarks.push(urlObj);
-    bookMarkCommon.paintingBookMark(urlObj);
-    bookMarkCommon.savingBookMark();
-  },
-  deleteBookMarkList: function (e) {
-    const li = e.target.parentElement;
-    const id = parseInt(li.id);
+      li.remove();
+      bookMarks = bookMarks.filter((bookMark) => bookMark.id !== id);
+      common.savingBookMark();
+    },
+  };
 
-    li.remove();
-    bookMarks = bookMarks.filter((bookMark) => bookMark.id !== id);
-    bookMarkCommon.savingBookMark();
-  },
-};
+  elements.$bookMarkIcon.addEventListener("click", handleEvent.clickIcon);
+  elements.$bookMarkForm.addEventListener(
+    "submit",
+    handleEvent.SubmitBookMarkForm
+  );
 
-bookMarkIcon.addEventListener("click", handleEventBookMark.clickIcon);
-bookMarkForm.addEventListener("submit", handleEventBookMark.SubmitBookMarkForm);
-
-const savedBookMarks = localStorage.getItem(BOOKMARK_KEY);
-bookMarkCommon.paintingSavedBookMarks();
+  common.paintingSavedBookMarks();
+})();
